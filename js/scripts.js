4c1f918724a39670ea5fe9c6743d7c9014872cc7
Elliot Jordan <ewjintl@icloud.com>
2020-01-31 14:01:34 -0800

    updates font styling in styles.css, encountered bug in frontend logic in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ function Order() {
   pizza = [];
   this.currentId = 0;
 }
-Order.prototype.addPizza = function(pizza) {
-var pizzaId = this.assignId();
-this.pizza.push(pizza);
+Order.prototype.addPizza = function (pizza) {
+  var pizzaId = this.assignId();
+  this.pizza.push(pizza);
 }
-Order.prototype.assignId = function() {
+Order.prototype.assignId = function () {
   this.currentId++;
   return this.currentId[i];
 }
-Order.prototype.findPizza = function(id) {
-  for (var i=0; i< this.Pizza.length; i++) {
+Order.prototype.findPizza = function (id) {
+  for (var i = 0; i < this.Pizza.length; i++) {
     if (this.Pizza[i]) {
       if (this.Pizza[i].id == id) {
         return this.Pizza[i];
@@ -29,8 +29,8 @@ function Pizza(size, sauce, crust, meats, veggies) {
   this.veggies = veggies;
   this.price = 0;
 }
-Order.prototype.whatSize = function() {
-  if (this.size === "small"){
+Order.prototype.whatSize = function () {
+  if (this.size === "small") {
     this.price += 6;
   } else if (this.size === "medium") {
     this.price += 9;
@@ -43,8 +43,8 @@ Order.prototype.whatSize = function() {
   }
   return this.price
 }
-Order.prototype.whatSauce = function() {
-  if (this.size === "red-tomato"){
+Order.prototype.whatSauce = function () {
+  if (this.size === "red-tomato") {
     this.price += 0;
   } else if (this.size === "white-sauce") {
     this.price += 2;
@@ -53,8 +53,8 @@ Order.prototype.whatSauce = function() {
   }
   return this.price
 }
-Order.prototype.whatCrust = function() {
-  if (this.size === "thin-crust"){
+Order.prototype.whatCrust = function () {
+  if (this.size === "thin-crust") {
     this.price -= 1;
   } else if (this.size === "hand-tossed") {
     this.price += 0;
@@ -62,35 +62,39 @@ Order.prototype.whatCrust = function() {
     this.price += 0;
   } else if (this.size === "stuffed") {
     this.price += 3;
-  return this.price
-}
-Order.prototype.whatMeats = function() {}
-Order.prototype.whatVeggies = function() {}
+    return this.price
+  }
+  // Order.prototype.whatMeats = function () { }
+  // Order.prototype.whatVeggies = function () { }
 
-// FRONTEND LOGIC //
-var newOrder = new Order();
+  // FRONTEND LOGIC //
+  var newOrder = new Order();
 
-$(document).ready(function(){
-  $("form#configurator").submit(function(event){
-    event.preventDefault();
+  $(document).ready(function () {
+    $("form#configurator").submit(function(event) {
+      event.preventDefault();
 
-    var selectedSize = $("select#pizza-size").val();
-    var selectedSauce = $("select#pizza-sauce").val();
-    var selectedCrust = $("select#pizza-crust").val();
-    var newPizza = new Pizza(selectedSize, selectedSauce, selectedCrust);
-    newOrder.addPizza(newPizza);
-    console.log(newOrder.Pizza);
+      var selectedSize = $("select#pizza-size").val();
+      var selectedSauce = $("select#pizza-sauce").val();
+      var selectedCrust = $("select#pizza-crust").val();
+      var newPizza = new Pizza(selectedSize, selectedSauce, selectedCrust);
+      newOrder.addPizza(newPizza);
+      console.log(newOrder.Pizza);
 
-    $("#meat-toppings").show();
-    $("input:checkbox[name=meat-toppings]:checked").each(function(){
-      var meats = $(this).val();
-      $('#meats').append(meats + "<br>");
+      $("#meat-toppings").show();
+      $("input:checkbox[name=meat-toppings]:checked").each(function () {
+        var meats = $(this).val();
+        $('#meats').append(meats + "<br>");
+      });
+
+      $("#veggie-toppings").show();
+      $("input:checkbox[name=veggie-toppings]:checked").each(function () {
+        var veggies = $(this).val();
+        $('#veggies').append(veggies + "<br>");
+      });
+
+      $('#configurator').hide();
     });
-    $("#veggie-toppings").show();
-    $("input:checkbox[name=veggie-toppings]:checked").each(function(){
-      var veggies = $(this).val();
-      $('#veggies').append(veggies + "<br>");
-    });
-    $('#configurator').hide();
   });
-});
+
+  
